18ea3c1af811a08b1ce01bb8a589affe90c055d3
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-01-09 19:01:23 +0700

    Extended lex to be able to tokenize numbers and indentifiers

--- sugiru/lexer/lexer.go
+++ sugiru/lexer/lexer.go
@@ -48,6 +48,8 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhiteSpace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -68,9 +70,78 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()          // Read the identifier
+			tok.Type = token.LookupIdent(tok.Literal) // Look up the identifier to get the appropriate token
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	// Advance to next character
 	l.readChar()
 	return tok
+}
+
+// isLetter returns whether a given byte represents a valid character
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+// isDigit returns whether a given byte is a digit
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// Return the identifier string
+func (l *Lexer) readIdentifier() string {
+
+	// Mark the beginning position of the identifier
+	position := l.position
+
+	// White the current letter is a valid letter, we read and consume
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	// Return the slice from beginning to current position
+	return l.fromPosToCurrent(position)
+}
+
+// Return the number as a string ( Note: fractions not currently supported )
+func (l *Lexer) readNumber() string {
+
+	// Mark the beginning of the lexeme
+	position := l.position
+
+	// Advance the character until we reach a non-digit
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	// Return the slice
+	return l.fromPosToCurrent(position)
+}
+
+// skipWhiteSpace consumes characters as long as it is a white space character
+func (l *Lexer) skipWhiteSpace() {
+	for {
+		switch l.ch {
+		case ' ', '\t', '\n', '\r':
+			l.readChar()
+		default:
+			return
+		}
+	}
+}
+
+// extractFromPosToCurrent retrieves a slice of the input, starting from position to current index
+func (l *Lexer) fromPosToCurrent(position int) string {
+	return l.input[position:l.position]
 }
--- sugiru/token/token.go
+++ sugiru/token/token.go
@@ -32,3 +32,20 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent returns the token type for the given identifier
+func LookupIdent(ident string) TokenType {
+
+	// Check if the given identifier is a language keyword
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	// If it isn't, we know it's just a plain identifier
+	return IDENT
+}
