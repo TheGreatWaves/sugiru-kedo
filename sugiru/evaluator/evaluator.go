006cc337ef6ed55df0c6d04ec6d35322398675c6
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-02-23 15:47:33 +0700

    evaluation: IntegerLiteral, BooleanLiteral, Null

---
+++ sugiru/evaluator/evaluator.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"sugiru/ast"
+	"sugiru/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	}
+
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
+}
