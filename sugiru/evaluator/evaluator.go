5b069ab6ac10b71058579d33111b514d274ca44b
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-03-01 20:15:38 +0700

    feat: bang impl

--- sugiru/evaluator/evaluator.go
+++ sugiru/evaluator/evaluator.go
@@ -31,9 +31,35 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return FALSE
+	default:
+		return FALSE
+	}
 }
 
 func evalStatements(statements []ast.Statement) object.Object {
